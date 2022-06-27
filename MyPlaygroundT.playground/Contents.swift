35fd4b2387bdc2137a83a241ce3aeb17e09394be
yvovk <frkvovk@gmail.com>
2022-06-27 14:30:17 +0300

    TasksBook

--- MyPlaygroundT.playground/Contents.swift
+++ MyPlaygroundT.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
+// Basic controll flow
 
-var greeting = "Hello, playground"
-2+2
+// 1 task
+var Counter = 0
+while Counter < 10 {
+    Counter = Counter + 1
+}
+print("Counter is \(Counter)")
 
+//2 task
+var roll = 0
+var counter = 0
+repeat {
+ roll = Int.random(in: 0 ... 5 )
+ counter += 1
+} while roll != 0
+print ("After  \(counter)  rolls, roll is \(roll) ")
